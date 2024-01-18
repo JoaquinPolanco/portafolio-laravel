2a191e6861986f0cef19d26abe040bf55a3396c7
JoaquinPolanco <joakinpolanco199726@gmail.com>
2024-01-18 14:07:24 -0600

    incio de portafolio

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joaquin Polanco</title>
+    <link rel="stylesheet" href="/CSS/styles.css">
+</head>
+<body>
+  <div class="cards-list">
+    <a class="card">
+      <div class="card_image">
+        <img src="/imgs/foto.jpeg" alt="Descripción de la imagen"/>
+      </div>
+      <div class="card_content">
+        <div class="card_title title-black">
+          <p>Lic. Joaquin Polanco</p>
+        </div>
+      </div>
+    </a>
+  </div>
+
+  <div class="social-container">
+    <div class="social-icons">
+      <a href="https://github.com/JoaquinPolanco" target="_blank" style="margin: 0 10px;"><img src="/imgs/github.png" alt="GitHub" style="width: 24px;"></a>
+      <a href="https://www.linkedin.com/in/joaquín-polanco199726/" target="_blank" style="margin: 0 10px;"><img src="/imgs/linkedin.png" alt="LinkedIn" style="width: 24px;"></a>
+      <a href="https://www.instagram.com/joakin17xd/" target="_blank" style="margin: 0 10px;"><img src="/imgs/instagram.png" alt="Instagram" style="width: 24px;"></a>
+      <a href="https://twitter.com/joaking17xD" target="_blank" style="margin: 0 10px;"><img src="/imgs/twitter.png" alt="Twitter" style="width: 24px;"></a>
+    </div>
+  </div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portafolio</title>
+    <link rel="stylesheet" href="{{ url('/css/styles.css') }}">
+
+
+</head>
+<body>
+
+<h1 class="text-center">Portafolio</h1>
+
+<div class="cards-list">
+    <a href="/#" class="card">
+        <div class="card_image">
+        
+        <img src="imgs/descarga.jpg" >
+        </div>
+        <div class="card_content">
+            <div class="card_title title-black">
+                <p>Licenciado En Sistemas Informaticos Administrativos</p>
+            </div>
+        </div>
+    </a>
+</div>
+
+</body>
+</html>
